467e3cb8223c4a020eec9fd11ec7a42c08a6a494
AbdulMoeed1996 <101922917+AbdulMoeed1996@users.noreply.github.com>
2022-05-14 20:02:01 +0500

    Add files via upload

---
+++ Suite.py
@@ -0,0 +1,52 @@
+import unittest
+import testcode1
+import testcode2
+
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+    def test_split(self):
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+        # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):
+            s.split(2)
+
+
+
+
+class TestCalc(unittest.TestCase):
+
+    def test_add(self):
+        self.assertEqual(testcode2.add(10, 5), 15)
+        self.assertEqual(testcode2.add(-1, 1), 0)
+        self.assertEqual(testcode2.add(-1, -1), -2)
+
+    def test_subtract(self):
+        self.assertEqual(testcode2.subtract(10, 5), 5)
+        self.assertEqual(testcode2.subtract(-1, 1), -2)
+        self.assertEqual(testcode2.subtract(-1, -1), 0)
+
+    def test_multiply(self):
+        self.assertEqual(testcode2.multiply(10, 5), 50)
+        self.assertEqual(testcode2.multiply(-1, 1), -1)
+        self.assertEqual(testcode2.multiply(-1, -1), 1)
+
+    def test_divide(self):
+        self.assertEqual(testcode2.divide(10, 5), 2)
+        self.assertEqual(testcode2.divide(-1, 1), -1)
+        self.assertEqual(testcode2.divide(-1, -1), 1)
+        self.assertEqual(testcode2.divide(5, 2), 2.5)
+
+        with self.assertRaises(ValueError):
+            testcode2.divide(10, 0)
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
